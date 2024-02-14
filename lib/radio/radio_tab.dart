b39d264ebe5bc86349574d85917b814f5a363b07
EslamShaaban <eslamshaaban01120@gmail.com>
2024-02-14 21:46:35 +0200

    edited splash_screen. finished sebha and radio tabs

--- lib/radio/radio_tab.dart
+++ lib/radio/radio_tab.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/app_theming.dart';
 
 class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/images/radio_device.png"),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25),
+          child: Text(
+            "إذاعة القرآن الكريم",
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 25,
+                ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.skip_previous,
+                  color: Theme.of(context).primaryColor,
+                  size: 50,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.play_arrow,
+                  color: Theme.of(context).primaryColor,
+                  size: 50,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.skip_next,
+                  color: Theme.of(context).primaryColor,
+                  size: 50,
+                )),
+          ],
+        )
+      ],
     );
   }
 }
